781d5857c28d085d707aa20d180cfe592fc2e343
saliou673 <diallom673@gmail.com>
2021-06-19 04:12:56 +0200

    add maven installer

---
+++ maven.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Before installing maven, ensure  java command is available.
+
+install_dir="/opt"
+current_dir=`pwd`
+maven_version="apache-maven-3.8.1"
+filename="${maven_version}-bin.tar.gz"
+download_path="https://mirror.ibcp.fr/pub/apache/maven/maven-3/3.8.1/binaries/${filename}"
+
+# Download and unzip package
+wget $download_path
+tar -xvf "${current_dir}/${filename}"
+
+# Move binarie to install directory
+output_path="${install_dir}/apachemaven"
+sudo mv $maven_version $output_path
+
+# Required environment variables
+m2_home="export M2_HOME=${output_path}"
+maven_home="export MAVEN_HOME=${output_path}"
+path="export PATH=\${M2_HOME}/bin:\${PATH}"
+
+# environment variable setting path
+env_file="/etc/profile.d/apachemaven.sh"
+
+sudo echo $m2_home >> $env_file
+sudo echo $maven_home >> $env_file
+sudo echo $path >> $env_file
+
+# Reload environment variables
+sudo chmod +x $env_file
+source $env_file
+
+rm "${current_dir}/${filename}"
+
+# verify installation
+mvn -version
+
+# Reboot for persisting new environment variables on all terminal
